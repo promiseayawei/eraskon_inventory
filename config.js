2022a2393151e0556d9171c44c48d26a90e8fcf4
Ayaweisoft <43746766+promiseayawei@users.noreply.github.com>
2025-05-17 19:50:48 -0400

    added product variant manager

---
+++ config.js
@@ -0,0 +1,3 @@
+
+const base_url = "http://localhost/KALPEP2/api3"; 
+const base_url_front = "http://localhost/KALPEP2";
--- config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$base_url = "http://localhost/KALPEP2/api3";
-$base_url_front = "http://localhost/KALPEP2";
