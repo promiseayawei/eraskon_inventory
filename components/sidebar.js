498a22e2d54b93ece51131e48bf62497d11bb046
Ayaweisoft <43746766+promiseayawei@users.noreply.github.com>
2025-05-17 20:19:52 -0400

    reuseable navbar and sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,28 +1,16 @@
-<!-- sidebar -->
-<link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-
-  <link rel="stylesheet" href="assets/css/style.css" />
-  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-  <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
-
-<nav class="sidebar">
+document.write(`
+  <!-- Sidebar -->
+  <nav class="sidebar">
     <div class="menu_content">
       <ul class="menu_items">
         <div class="menu_title menu_dahsboard"></div>
-        <!-- duplicate or remove this li tag if you want to add or remove navlink with submenu -->
-        <!-- start -->
+
         <li class="item">
           <div href="#" class="nav_link submenu_item">
-            <span class="navlink_icon">
-              <i class="bx bx-home-alt"></i>
-            </span>
+            <span class="navlink_icon"><i class="bx bx-home-alt"></i></span>
             <span class="navlink">Home</span>
             <i class="bx bx-chevron-right arrow-left"></i>
           </div>
-
           <ul class="menu_items submenu">
             <a href="#" class="nav_link sublink">Nav Sub Link</a>
             <a href="#" class="nav_link sublink">Nav Sub Link</a>
@@ -30,19 +18,13 @@
             <a href="#" class="nav_link sublink">Nav Sub Link</a>
           </ul>
         </li>
-        <!-- end -->
 
-        <!-- duplicate this li tag if you want to add or remove  navlink with submenu -->
-        <!-- start -->
         <li class="item">
           <div href="#" class="nav_link submenu_item">
-            <span class="navlink_icon">
-              <i class="bx bx-grid-alt"></i>
-            </span>
+            <span class="navlink_icon"><i class="bx bx-grid-alt"></i></span>
             <span class="navlink">Overview</span>
             <i class="bx bx-chevron-right arrow-left"></i>
           </div>
-
           <ul class="menu_items submenu">
             <a href="#" class="nav_link sublink">Nav Sub Link</a>
             <a href="#" class="nav_link sublink">Nav Sub Link</a>
@@ -50,60 +32,46 @@
             <a href="#" class="nav_link sublink">Nav Sub Link</a>
           </ul>
         </li>
-        <!-- end -->
       </ul>
 
       <ul class="menu_items">
         <div class="menu_title menu_editor"></div>
-        <!-- duplicate these li tag if you want to add or remove navlink only -->
-        <!-- Start -->
         <li class="item">
           <a href="#" class="nav_link">
-            <span class="navlink_icon">
-              <i class="bx bxs-magic-wand"></i>
-            </span>
+            <span class="navlink_icon"><i class="bx bxs-magic-wand"></i></span>
             <span class="navlink">Products</span>
           </a>
         </li>
-        <!-- End -->
-
         <li class="item">
           <a href="#" class="nav_link">
-            <span class="navlink_icon">
-              <i class="bx bx-loader-circle"></i>
-            </span>
+            <span class="navlink_icon"><i class="bx bx-loader-circle"></i></span>
             <span class="navlink">Sales</span>
           </a>
         </li>
         <li class="item">
           <a href="#" class="nav_link">
-            <span class="navlink_icon">
-              <i class="bx bx-filter"></i>
-            </span>
+            <span class="navlink_icon"><i class="bx bx-filter"></i></span>
             <span class="navlink">Report</span>
           </a>
         </li>
         <li class="item">
           <a href="#" class="nav_link">
-            <span class="navlink_icon">
-              <i class="bx bx-cloud-upload"></i>
-            </span>
-            <span class="navlink">stock</span>
+            <span class="navlink_icon"><i class="bx bx-cloud-upload"></i></span>
+            <span class="navlink">Stock</span>
           </a>
         </li>
       </ul>
 
-
-      <!-- Sidebar Open / Close -->
       <div class="bottom_content">
         <div class="bottom expand_sidebar">
-          <span> Expand</span>
+          <span>Expand</span>
           <i class='bx bx-log-in'></i>
         </div>
         <div class="bottom collapse_sidebar">
-          <span> Collapse</span>
+          <span>Collapse</span>
           <i class='bx bx-log-out'></i>
         </div>
       </div>
     </div>
   </nav>
+`);
