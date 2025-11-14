e89e9e0c1f6a8e7081d2a10bb6a4ce54bee1403a
promiseayawei <ayaweisoft@gmail.com>
2025-11-14 08:12:22 -0500

    major updates railway deploy

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ const base_url_front = "http://localhost/KALPEP2";
 // const base_url = "http://localhost:5000"; // For local development
  //window.BASE_URL = "http://localhost:5000";
 // const base_url = "https://eraskon-server.onrender.com"; // For production
-window.BASE_URL = "https://eraskon-server.onrender.com";
+//window.BASE_URL = "https://eraskon-server.onrender.com";
+window.BASE_URL = "https://eraskonserver-production.up.railway.app";
 
