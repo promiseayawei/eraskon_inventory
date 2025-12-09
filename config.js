60b83deab1890ca4e71a430d5d252b72a41c5df6
promiseayawei <ayaweisoft@gmail.com>
2025-12-09 16:10:51 -0500

    stock

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-const base_url = "https://eraskon-server.onrender.com"; 
-const base_url_front = "http://localhost/KALPEP2";
-// const base_url = "http://localhost:5000"; // For local development
- //window.BASE_URL = "http://localhost:5000";
-// const base_url = "https://eraskon-server.onrender.com"; // For production
-//window.BASE_URL = "https://eraskon-server.onrender.com";
-// window.BASE_URL = "https://eraskonserver-production.up.railway.app";
+// config.js
+
 window.BASE_URL = "https://api.eraskon.com";
 
