6b28aa459339f69f03fe17ae1e506992e51b3c3a
promiseayawei <ayaweisoft@gmail.com>
2025-12-11 23:43:43 -0500

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 
-//window.BASE_URL = "https://api.eraskon.com";
-window.BASE_URL = "http://localhost:5000";
+window.BASE_URL = "https://api.eraskon.com";
+//window.BASE_URL = "http://localhost:5000";
 
