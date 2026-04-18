95001a99fac7035ac29c6abcb80e4f0413030248
promiseayawei <ayaweisoft@gmail.com>
2026-04-18 12:58:36 -0400

    config

--- www/config.js
+++ www/config.js
@@ -1,4 +1,4 @@
-const base_url = "https://eraskon-server.onrender.com"; 
+const base_url = "https://api.eraskon.com"; 
 // const base_url = "http://localhost:5000"; // For local development
 window.BASE_URL = "https://api.eraskon.com";
 //  window.BASE_URL = "http://localhost:5000";
